280535d9e445bd2a84385425ac5750266af74a4a
Joseph Hu <josephhu007@gmail.com>
2023-11-22 21:29:53 -0800

    complete front-end design overhaul of home page and implement search bar functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from './components/SearchBar';
 import SignInButton from './components/SignInButton';
+import SearchOutput from './components/SearchOutput';
 
 const App = () => {
+    const [results, setResults] = useState([]);
     return (
-        <div>
-            <div class="logo"> BruinBytes </div>
-            <div class="searchbar-and-signin">
-                <SearchBar />
+        <div className="overall">
+            <div className="header-group">
+                <div className="logo">BruinBytes</div>
+                <SearchBar setResults={setResults} />
                 <SignInButton />
+            </div>
+            <SearchOutput results={results} />
+            <div className="image-group">
+                <img src="" alt="Description" className="restaurant1" />
             </div>
         </div>
     );
---
+++ src/components/SearchOutput.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../design.css";
+
+/*
+List of search results
+*/
+export const SearchOutput = ({results}) => {
+    return (
+        <div className="all-results">
+            {results.map(result => <SomeResult result={result} key={result.id} />)}
+        </div>
+    );
+}
+
+/*
+For each search result
+*/
+const SomeResult = ({result}) => {
+    const handleClick = () => {
+        alert(`Need to implement page for: ${result.name}`);
+    };
+
+    return (
+        <div className="one-search-result" onClick={handleClick}>
+            {result.name}
+        </div>
+    );
+}
+
+export default SearchOutput;
