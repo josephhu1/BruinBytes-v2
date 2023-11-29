1206de2424491f5304088441c7eb55d6bdf44765
Joseph Hu <josephhu007@gmail.com>
2023-11-28 16:51:53 -0800

    add melis_branch review page files with joseph-branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,127 @@
 import React, { useState } from 'react';
-import SearchBar from './components/SearchBar';
-import SignInButton from './components/SignInButton';
-import SearchOutput from './components/SearchOutput';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+import './styles.css';
 
-const App = () => {
-    const [results, setResults] = useState([]);
-    return (
-        <div className="overall">
-            <div className="header-group">
-                <div className="logo">BruinBytes</div>
-                <SearchBar setResults={setResults} />
-                <SignInButton />
-            </div>
-            <SearchOutput results={results} />
-            <div className="image-group">
-                <img src="" alt="Description" className="restaurant1" />
-            </div>
+
+import { AiFillStar, AiOutlineStar} from "react-icons/ai";
+
+function App() {
+  const [number, setNumber] = useState(0);
+
+  const [name, setName] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  
+
+  const handleText = () => {
+    switch(number){
+      case 0:
+        return 'Evaluate';
+      case 1:
+        return 'Dissatisfaction';
+      case 2:
+        return 'Not fine';
+      
+      case 3:
+        return 'Fine';
+
+      case 4:
+        return 'Good';
+
+      case 5:
+        return 'Excellent';
+
+      default:
+        return 'Evaluate';
+
+    }
+  }
+
+
+  return (
+    <div className="App">
+      <div className="popup">
+        <h1 style={{ color: 'orange' }}>Restaurant Detail</h1>
+
+      
+        
+        <div className="content">
+          <div className="product">
+              
+              { <img src={"https://www.google.com/url?sa=i&url=https%3A%2F%2Fconferences.ucla.edu%2Fa-history-of-ucla-logos%2F&psig=AOvVaw0GXoJSRQS5hBb8Xh6-NqDU&ust=1701045073967000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCKjP4ey04IIDFQAAAAAdAAAAABAE"} alt="Logo"/> }
+              <div className="restaurant-container">
+                <div className="restaurant-box">
+                  <h2>Restaurant Name1</h2>
+                 <p>Name: Ryan - Rating: 5</p>
+                 <p>Comment: Excellent Restaurant</p>
+
+                </div>
+
+                <div className="restaurant-box">
+                  <h2>Restaurant Name2</h2>
+                  <p>Name: Sam - Rating: 3</p>
+                  <p>Comment: Cheap but not good</p>
+
+                </div>
+                <div className="restaurant-box">
+                  <h2>Restaurant Name3</h2>
+                  <p>Name: Mel - Rating: 2</p>
+                  <p>Comment: Not clean</p>
+
+                </div>  
+              </div>
+          </div>
+
+          
+      </div>
+
+        <div>
+          <h1>{handleText()}</h1>
+          {Array(5).fill().map((_,index)=>
+            number >= index +1 ?(
+
+            <AiFillStar style = {{color:'orange' }} 
+            onClick={()=>setNumber(index+1) }/>
+            
+
+            ): (
+
+            <AiOutlineStar style={{color:'orange'}} 
+            onClick={()=>setNumber(index+1) }/>
+            )
+          
+
+          )}
+
+          
+          
+          
         </div>
-    );
-};
+
+        <div>
+
+        <input 
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Enter your name"
+          /> 
+
+        </div>
+
+        <textarea placeholder="Enter Comment..."> </textarea>
+
+        
+        <button className={'${!number && "disabled"}'}>Submit</button>
+      </div>
+      
+    </div>
+  );
+}
 
 export default App;
---
+++ src/App1.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar';
+import SignInButton from './components/SignInButton';
+import SearchOutput from './components/SearchOutput';
+
+const App = () => {
+    const [results, setResults] = useState([]);
+    return (
+        <div className="overall">
+            <div className="header-group">
+                <div className="logo">BruinBytes</div>
+                <SearchBar setResults={setResults} />
+                <SignInButton />
+            </div>
+            <SearchOutput results={results} />
+            <div className="image-group">
+                <img src="" alt="Description" className="restaurant1" />
+            </div>
+        </div>
+    );
+};
+
+export default App;
